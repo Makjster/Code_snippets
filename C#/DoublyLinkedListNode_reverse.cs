df85be64da90cc252eba1549f3ba97ef36d81d7b
Makjster <Matti.Johannsen@gmx.de>
2021-01-13 11:41:17 +0100

    Create DoublyLinkedListNode_reverse.cs

---
+++ C#/DoublyLinkedListNode_reverse.cs
@@ -0,0 +1,17 @@
+static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+        
+        DoublyLinkedListNode temp = head;
+        DoublyLinkedListNode newHead = head;
+        
+        while(temp!=null){
+            
+            DoublyLinkedListNode prev = temp.prev;
+            temp.prev = temp.next;
+            temp.next = prev;
+            newHead = temp;
+            temp = temp.prev;
+        }
+    
+        return newHead;
+    
+    }
